2b80aee186b83f685b604e057a39c25989947b29
Nandini Talwar <talwarn@amazon.com>
2021-10-09 23:39:12 -0400

    initial commit

---
+++ my-bot/handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.getWeather = async (event) => {
+  return {
+    
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
